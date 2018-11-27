573501379e0f6ae2f7a76b75c8bf729130e9fbbd
brooke <brooke.mitchell@comcast.net>
2018-11-27 18:28:47 -0500

    some edits

--- public/js/index.js
+++ public/js/index.js
@@ -3,15 +3,14 @@ var $itemText = $("#item-text");
 var $itemDescription = $("#description");
 var $itemCategory = $("#category");
 var $itemDuration = $("#duration");
-var $addRow = $('.addRow')
+var $addRow = $(".addRow");
 var $submitBtn = $("#submitBtn");
 var $itemList = $("#item-list");
 var $toDoListItem = $("#toDoListItem");
 
-
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveItem: function (item) {
+  saveItem: function(item) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -21,13 +20,13 @@ var API = {
       data: JSON.stringify(item)
     });
   },
-  getItems: function () {
+  getItems: function() {
     return $.ajax({
       url: "/api/items",
       type: "GET"
     });
   },
-  deleteItem: function (id) {
+  deleteItem: function(id) {
     return $.ajax({
       url: "/api/items/" + id,
       type: "DELETE"
@@ -35,11 +34,10 @@ var API = {
   }
 };
 
-
 // refreshItems gets new items from the db and repopulates the list
-var refreshItems = function () {
-  API.getItems().then(function (data) {
-    var $items = data.map(function (item) {
+var refreshItems = function() {
+  API.getItems().then(function(data) {
+    var $items = data.map(function(item) {
       var $a = $("<a>")
         .text(item.text)
         .attr("href", "/item/" + item.id);
@@ -65,10 +63,9 @@ var refreshItems = function () {
   });
 };
 
-
 // handleFormSubmit is called whenever we submit a new list
 // Save the new list to the db and refresh the list
-var handleFormSubmit = function (event) {
+var handleFormSubmit = function(event) {
   event.preventDefault();
 
   var item = {
@@ -79,7 +76,7 @@ var handleFormSubmit = function (event) {
     toDoListItem: $toDoListItem.val().trim()
   };
 
-  API.saveItem(item).then(function () {
+  API.saveItem(item).then(function() {
     refreshItems();
   });
 
@@ -90,32 +87,52 @@ var handleFormSubmit = function (event) {
   $toDoListItem.val("");
 };
 
-
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function () {
+var handleDeleteBtnClick = function() {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteItem(idToDelete).then(function () {
+  API.deleteItem(idToDelete).then(function() {
     refreshItems();
   });
 };
 
-
 // Logic for adding another row for items
-$(function(){
+$(function() {
   var count = 0;
-  $('.addRow').click(function(){
-    $('#inputFields').append('<div class="form-group"> <input type="text" id="toDoListItem'+count+'" class="form-control" placeholder="What else do you want to do?"></div>');
+  $(".addRow").click(function() {
+    $("#inputFields").append(
+      '<div class="form-group"> <input type="text" id="toDoListItem' +
+        count +
+        '" class="form-control" placeholder="What else do you want to do?"></div>'
+    );
     count++;
     // Test that count variable is working
     console.log(count);
   });
 });
 
-
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $itemList.on("click", ".delete", handleDeleteBtnClick);
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 50) {
+    $(".scrollToTop:hidden")
+      .stop(true, true)
+      .fadeIn();
+  } else {
+    $(".scrollToTop")
+      .stop(true, true)
+      .fadeOut();
+  }
+});
+$(function() {
+  $(".scroll").click(function() {
+  console.log("clicked");
+    $("html, body").animate({ scrollTop: $(".navbar").offset().top }, "1000");
+    return false;
+  });
+});
